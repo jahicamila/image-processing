ed230829d26b2de9a0a4ff43bb35b5332f307a94
Amila Jahić <102929605+jahicamila@users.noreply.github.com>
2023-07-05 00:05:06 +0200

    Create morphological-operations.py

---
+++ morphological-processing/morphological-operations.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+path = os.path.join('..', os.getcwd(), 'images', 'coins.jpg')
+img  = cv.imread(path, flags=0) # flag 0 for grayscale image
+
+(ret, thresh) = cv.threshold(img, 100, 255, cv.THRESH_BINARY_INV)
+kernel = np.ones((5,5), np.uint8)
+
+img_erosion = cv.erode(thresh, kernel, iterations=1)
+img_dilation = cv.dilate(thresh, kernel, iterations=1)
+img_opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
+img_closing = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+
+plt.subplot(2,3,1)
+plt.axis('off')
+plt.title('Original')
+plt.imshow(img, cmap='gray')
+
+plt.subplot(2,3,2)
+plt.axis('off')
+plt.title('Binary')
+plt.imshow(thresh, cmap='binary')
+
+plt.subplot(2,3,3)
+plt.axis('off')
+plt.title('Erosion')
+plt.imshow(img_erosion, cmap='binary')
+
+plt.subplot(2,3,4)
+plt.axis('off')
+plt.title('Dilation')
+plt.imshow(img_dilation, cmap='binary')
+
+plt.subplot(2,3,5)
+plt.axis('off')
+plt.title('Opening')
+plt.imshow(img_opening, cmap='binary')
+
+plt.subplot(2,3,6)
+plt.axis('off')
+plt.title('Closing')
+plt.imshow(img_closing, cmap='binary')
+
+plt.show()
